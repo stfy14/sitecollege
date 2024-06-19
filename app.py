811dba0f4d891c8f316579b2ed154934b1b782bb
stfy14 <teamft2005@gmail.com>
2024-06-20 02:46:35 +0500

    small edit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, make_response
 from flask_login import LoginManager, login_user, current_user, logout_user
 from werkzeug.security import check_password_hash, generate_password_hash
 from models import User, Contacts, db
@@ -19,6 +19,10 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 @app.route('/')
 def index():
@@ -78,9 +82,13 @@ def logout():
 
 @app.route('/contacts', methods=['GET'])
 def contacts():
-    import_data_from_url(url='https://odatauser:odatapass@it.usaaa.ru/dev-5/odata/standard.odata/Catalog_ФизическиеЛица?$format=json')
     contacts = Contacts.query.all()
     return render_template('contacts.html', contacts=contacts)
+
+@app.route('/api/get_contacts', methods=['GET'])
+def get_contacts():
+    import_data_from_url(url='https://odatauser:odatapass@it.usaaa.ru/dev-5/odata/standard.odata/Catalog_ФизическиеЛица?$format=json')
+    return make_response(jsonify({'status': 'done'}), 200)
 
 if __name__ == '__main__':
     with app.app_context():
